2ced96a287f2e22acbdef24aab86eed5ccfd5481
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-04-01 12:58:56 +0200

    infinite collision

--- script.js
+++ script.js
@@ -8,17 +8,70 @@ let ctx = canvas.getContext('2d')
 let startBtn = document.querySelector('#start')
 let restartBtn = document.querySelector('#restart')
 
+let circleX = 50, circleY = 50, circleRadius = 30;
+let incrX = 5;
+let incrY = 5;
+let isGameOver = false;
+let itervalId = 0;
 
+function drawCircle() {
+    ctx.beginPath()
+    ctx.fillStyle = "#19fae0"
+    ctx.arc(circleX, circleY, 30, 0, 2*Math.PI)
+    ctx.fill()
+    ctx.closePath()
+}
+
+function collision(){
+    if(circleX >canvas.width - circleRadius){
+        incrX = -5
+    }
+    if(circleY >canvas.height - circleRadius){
+        incrY = -5
+    }
+    if(circleX<circleRadius){
+        incrX = 5
+    }
+    if(circleY<circleRadius){
+        incrY = 5
+    }
+}
+
+function animate(){
+    ctx.clearRect(0,0,canvas.width, canvas.height)
+    drawCircle()
+    collision()
+    circleX += incrX
+    circleY += incrY
+    requestAnimationFrame(animate)
+    if(isGameOver){
+        cancelAnimationFrame()
+
+    }else{
+        intervalId = requesAnimationFrame(animate)
+    }
+}
+
+function start(){
+    canvas.style.display = 'block'
+    startBtn.style.display = 'none'
+    animate()
+}
+
+function restart() {
+
+}
 
  //Everything begins here
 window.addEventListener('load', () => {
-   
-    
+    canvas.style.display = 'none'
+    restartBtn.style.display = 'none'
+    start()
     startBtn.addEventListener('click', () => {
-        // do something when the user clicks the start button
+         start()
     })
 
     restartBtn.addEventListener('click', () => {
-        // do something when the user clicks the restart button
+        restart()
     })
 })
